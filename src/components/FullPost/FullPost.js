115460b1c5775ee0ae9089f3e2738206a70adcb6
Air <Air@Air.local>
2019-08-22 13:41:47 +0800

    Add axios global defaults

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -11,14 +11,14 @@ class FullPost extends Component {
     componentDidUpdate() {
       if (this.props.id) {
         if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-          axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+          axios.get('/posts/' + this.props.id)
             .then(response => this.setState({ loadedPost: response.data}));
         }
       }
     }
 
     deletePostHandler = () => {
-      axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+      axios.get('/posts/' + this.props.id)
         .then(response => console.log(response));
     }
 
--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -13,7 +13,7 @@ class NewPost extends Component {
     postDataHandler = () => {
       const data = {...this.state};
 
-      axios.post('https://jsonplaceholder.typicode.com/posts', data)
+      axios.post('/posts', data)
         .then(response => {
           console.log(response);
         });
