8789626832339494012ba0a1091dec8f5b3cfb4a
Air <Air@Air.local>
2019-08-26 16:10:06 +0800

    Add switch to routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -33,9 +33,12 @@ class Blog extends Component {
                 </ul>
               </nav>
             </header>
+            {/* Routes */}
             <Route path="/" exact component={Posts} />
-            <Route path="/new-post" exact component={NewPost} />
-            <Route path="/:id" exact component={FullPost} />
+            <Switch>
+              <Route path="/new-post" component={NewPost} />
+              <Route path="/:id" exact component={FullPost} />
+            </Switch>
           </div>
       );
   }
