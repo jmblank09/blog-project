3e84d55cbaf6d702b56878cd26da9a94e9e22cd6
Air <Air@Air.local>
2019-08-26 19:31:01 +0800

    Add redirect route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import {
+  Route,
+  Switch,
+  NavLink,
+  Redirect
+} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -36,6 +41,8 @@ class Blog extends Component {
             <Switch>
               <Route path="/new-post" component={NewPost} />
               <Route path="/posts" component={Posts} />
+              <Redirect from="/" to="/posts" />
+              {/* <Route path="/" component={Posts} />  */}
             </Switch>
           </div>
       );
